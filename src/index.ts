f711bbf194c9d495f1fc1d036abe9c2832fd3d5e
pabin-limbu <changsu.pabin@gmail.com>
2024-03-12 15:27:44 +0800

    user Schema created

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database")); // connect mongo db with connection string and make sure the connection string are string.
 
 const app = express(); // create express server.
 app.use(express.json()); // use middleware to parse every incoming request to json.
 app.use(cors());
+
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "hello" });
+});
+
+app.listen(7000, () => {
+  console.log("Server started on localhost:7000");
 });
---
+++ src/models/user.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  auth0Id: { type: String, require: true },
+  email: { type: String },
+  name: { type: String },
+  addressLine1: { type: String },
+  city: { type: String },
+  country: { type: String },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
