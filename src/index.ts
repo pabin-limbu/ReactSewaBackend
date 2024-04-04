31b233574f90965ffed68306b849046249a1aed9
pabin-limbu <changsu.pabin@gmail.com>
2024-04-04 21:05:14 +0800

    Health api added

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,11 @@ const app = express(); // create express server.
 app.use(express.json()); // use middleware to parse every incoming request to json.
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health ok!" }); // the convention to check if the server is running .
+});
 app.use("/api/my/user", myUserRoute);
+
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
 });
